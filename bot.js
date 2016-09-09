15975da6cfbabc0f27c5e96a9a3ea1ffcd30da26
Christopher Plantijn <cplantijn90@gmail.com>
2016-09-09 02:50:45 -0400

    Step 1 Ready

---
+++ bot.js
@@ -0,0 +1,50 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+var express = require('express');
+var app = express();
+var server = require('http').Server(express);
+var io = require('socket.io')(server);
+var path = require("path");
+
+/* Board Settings
+- White - Left - 6
+- Green - Forwards - 7
+- Yellow - Backwards - 8
+- Blue - Right - 9
+*/
+
+var ON = 0;
+var OFF = 1;
+
+var leftPin = 6,
+    rightPin = 9,
+    backwardsPin = 8,
+    forwardsPin = 7;
+
+board.on('ready', function() {
+  this.pinMode(leftPin, five.Pin.OUTPUT);
+  this.pinMode(rightPin, five.Pin.OUTPUT);
+  this.pinMode(forwardsPin, five.Pin.OUTPUT);
+  this.pinMode(backwardsPin, five.Pin.OUTPUT);
+  stopAll();
+});
+
+function stopAll() {
+  board.digitalWrite(leftPin, OFF);
+  board.digitalWrite(rightPin, OFF);
+  board.digitalWrite(forwardsPin, OFF);
+  board.digitalWrite(backwardsPin, OFF);
+}
+
+
+// Stuff to start the server!
+app.use('/client', express.static('client'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+server.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
